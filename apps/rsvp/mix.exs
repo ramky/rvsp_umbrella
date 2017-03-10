c311008aa1dd91a53d33adb3d9da8039826b53e4
Ram Iyer <ram.iyer@bydeluxe.com>
2017-03-10 01:49:49 -0600

    adding ecto

--- apps/rsvp/mix.exs
+++ apps/rsvp/mix.exs
@@ -15,7 +15,7 @@ defmodule Rsvp.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger],
+    [extra_applications: [:logger, :postgrex, :ecto],
      mod: {Rsvp.Application, []}]
   end
 
@@ -29,6 +29,9 @@ defmodule Rsvp.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:postgrex, "~> 0.13.2"},
+      {:ecto, "~> 2.1"}
+    ]
   end
 end
