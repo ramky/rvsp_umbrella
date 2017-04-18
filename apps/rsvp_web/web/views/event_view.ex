351c9d829775c5b246dd0904a71a50d1a4724a37
Ram Iyer <ram.iyer@bydeluxe.com>
2017-04-17 19:11:49 -0500

    displaying the desc and time correctly on the events show action

--- apps/rsvp_web/web/views/event_view.ex
+++ apps/rsvp_web/web/views/event_view.ex
@@ -2,7 +2,7 @@ defmodule RsvpWeb.EventView do
   use RsvpWeb.Web, :view
 
   def format_date(date) do
-    {{y,m,d}, _} = Ecto.DateTime.to_erl(date)
-    "#{m}/#{d}/#{y}"
+    {{y,m,d}, {h,mm,s}} = Ecto.DateTime.to_erl(date)
+    "#{m}/#{d}/#{y} at #{h}:#{mm}:#{s}"
   end
 end
